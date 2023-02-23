8525527f9df8af10f6b69b3d01bb18190715d579
= <=>
2023-02-23 04:05:10 -0300

    correção na logica

--- logica_de_programacao/exercicios/imc/assets/js/main.js
+++ logica_de_programacao/exercicios/imc/assets/js/main.js
@@ -1,16 +1,8 @@
-const validators = (p,a,r) =>{
-    if (p ||a !== Number){
+const styleValidators = (r) =>{
+        r.innerHTML = "valor invalido"
         r.style.backgroundColor = "#f53944";
         r.style.textAlign = "center";
         r.style.borderRadius = "5px";
-        r.innerHTML = "valor invalido"
-    }else
-    if(p > 595 && a > 250){
-        r.style.backgroundColor = "#f53944";
-        r.style.textAlign = "center";
-        r.style.borderRadius = "5px";
-        r.innerHTML = "valor invalido"
-    }
 }
 
 
@@ -30,7 +22,7 @@ const styleshetsResp = (p,a,r) =>{
         r.style.textAlign = "center";
         r.style.borderRadius = "5px";
     }else
-    if(calcImc(p,a) >= 30 ){
+    if(calcImc(p,a) > 29.9 ){
         r.style.backgroundColor = "#f53944";
         r.style.textAlign = "center";
         r.style.borderRadius = "5px";
@@ -38,6 +30,7 @@ const styleshetsResp = (p,a,r) =>{
         r.style.backgroundColor = "#5eba7d";
         r.style.textAlign = "center"
         r.style.borderRadius = "5px";
+        console.log("cai aqui ?")
     }
 
 }
@@ -47,33 +40,39 @@ const imc = () =>{
     const resp = document.querySelector("#resp");
     formulario.addEventListener("submit", function(event){
         event.preventDefault()
-        const peso = formulario.querySelector(".peso").value;
-        const altura = formulario.querySelector(".altura").value; 
-        validators(peso,altura,resp)
-        
+        const peso = parseFloat(formulario.querySelector(".peso").value);
+        const altura = parseFloat(formulario.querySelector(".altura").value); 
+
+        if(!peso || !altura){
+            return styleValidators(resp)
+        }else
+        if(peso < 25 || peso > 595 || altura < 59 || altura >250 ){
+            return styleValidators(resp)
+        }else
         if(calcImc(peso,altura) < 18.5){
             styleshetsResp(peso,altura,resp)
-            resp.innerHTML = `seu IMC É ${calcImc(peso,altura).toFixed(2)}, você esta abaixo do peso`;
+           return resp.innerHTML = `seu IMC É ${calcImc(peso,altura).toFixed(2)}, você esta abaixo do peso`;
         }else
-        if(calcImc(peso,altura) >=18.5 && calcImc(peso,altura) <= 24.9){
+        if(calcImc(peso,altura) >=18.5 & calcImc(peso,altura) <= 24.9){
             styleshetsResp(peso,altura,resp)
-            resp.innerHTML = `seu IMC É ${calcImc(peso,altura).toFixed(2)}, seu peso esta normal`;
+            return resp.innerHTML = `seu IMC É ${calcImc(peso,altura).toFixed(2)}, seu peso esta normal`;
         }else
         if(calcImc(peso,altura) >=25 && calcImc(peso,altura) <= 29.9){
             styleshetsResp(peso,altura,resp)
-            resp.innerHTML = `seu IMC É ${calcImc(peso,altura).toFixed(2)}, Sobrepeso`;
+           return resp.innerHTML = `seu IMC É ${calcImc(peso,altura).toFixed(2)}, Sobrepeso`;
         }else
-        if(calcImc(peso,altura) >=30 && calcImc(peso,altura) <= 34.9){
+        if(calcImc(peso,altura) >29.9 && calcImc(peso,altura) <= 34.9){
             styleshetsResp(peso,altura,resp)
-            resp.innerHTML = `seu IMC É ${calcImc(peso,altura).toFixed(2)}, Obesidade grau 1`;
+           return resp.innerHTML = `seu IMC É ${calcImc(peso,altura).toFixed(2)}, Obesidade grau 1`;
         }else
         if(calcImc(peso,altura) >=35 && calcImc(peso,altura) <= 39.9){
             styleshetsResp(peso,altura,resp)
-            resp.innerHTML = `seu IMC É ${calcImc(peso,altura).toFixed(2)}, Obesidade grau 2`;
+            return resp.innerHTML = `seu IMC É ${calcImc(peso,altura).toFixed(2)}, Obesidade grau 2`;
         }
         else{
-            styleshetsResp(peso,resp)
-            resp.innerHTML = `seu IMC É ${calcImc(peso,altura).toFixed(2)}, Obesidade grau 3`;
+            styleshetsResp(peso,altura,resp)
+            console.log("cai aqui")
+            return resp.innerHTML = `seu IMC É ${calcImc(peso,altura).toFixed(2)}, Obesidade grau 3`;
         }
     });
 }
